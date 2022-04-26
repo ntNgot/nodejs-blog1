d2599173b58fddc98716b938dc29fd11f4e24a98
Ngoc Thao - 165102 <nt.280420@gmail.com>
2022-04-26 15:21:08 +0700

    commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express()
 const port = 300
+
+app.use(morgan('combined'))
 
 app.get('/tin-tuc', (req, res) => {
     res.send("hello")
 })
-
 app.listen(port, () => {
     console.log(`example app listening on port ${port}`)
 })
