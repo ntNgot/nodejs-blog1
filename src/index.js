21d0d81cc72af59f1337c452359ed78d4b1dd7a3
Ngoc Thao - 165102 <nt.280420@gmail.com>
2022-04-26 16:43:26 +0700

    commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
-const express = require('express')
-const morgan = require('morgan')
+const path = require('path')
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
 
-const app = express()
-const port = 300
+const app = express();
+const port = 300;
 
-app.use(morgan('combined'))
+//HTTP Logger
+app.use(morgan('combined'));
+
+//Template engine
+app.engine('handlebars', handlebars.engine());
+app.set('view engine', 'handlebars')
+app.set('views', path.join(__dirname, 'resources/views'))
+
+console.log("Path: " + path.join(__dirname, 'resources/views'));
 
 app.get('/tin-tuc', (req, res) => {
-    res.send("hello")
+    res.render('home');
 })
 app.listen(port, () => {
-    console.log(`example app listening on port ${port}`)
+    console.log(`example app listening on port ${port}`);
 })
