46d2dff43d4f7a99c7e78c97d8237e3c4eb970cf
Ngoc Thao - 165102 <nt.280420@gmail.com>
2022-05-17 23:18:27 +0700

    tích hợp scss

--- src/index.js
+++ src/index.js
@@ -2,23 +2,35 @@ const path = require('path')
 const express = require('express');
 const morgan = require('morgan');
 const handlebars = require('express-handlebars');
-
 const app = express();
 const port = 300;
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 //HTTP Logger
 app.use(morgan('combined'));
 
 //Template engine
-app.engine('handlebars', handlebars.engine());
-app.set('view engine', 'handlebars')
+app.engine('hbs', handlebars.engine({
+    extname: '.hbs'
+}));
+app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'resources/views'))
 
 console.log("Path: " + path.join(__dirname, 'resources/views'));
 
-app.get('/tin-tuc', (req, res) => {
+app.get('/trang-chu', (req, res) => {
     res.render('home');
 })
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+app.get('/tin-tuc', (req, res) => {
+    res.render('news');
+})
+
 app.listen(port, () => {
     console.log(`example app listening on port ${port}`);
 })
